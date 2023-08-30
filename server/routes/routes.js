62e646d90524abec0d872c41f46503f7b58df0e2
Brian Mbugua <brianmbugua505@gmail.com>
2023-08-30 16:48:25 +0300

    implemented new user registration through postman

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,42 @@
-//define all api endpoints
-
 const express = require('express');
-const router = express.Router()
+const {Router} = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+const router = Router();
 
 
 router.get('/', (req, res) => {
-    res.send('From API route')
+    res.send('Server root OK')
 })
 
-router.post('/register', (req, res) => {
-    let userData = req.body;
+router.post('/register', async(req, res) => {
+   let username = req.body.username
+   let email = req.body.email
+   let password = req.body.password
+
+   const user = new User({
+    username: username,
+    email: email,
+    password: password
+   })
+
+   const result = await user.save()
+
+   res.json({
+    user:result
+   })
+
+})
+
+router.post('/login', async(req, res) => {
+    res.send("Login endpoint OK")
+})
+
+router.get('/user', async(req, res) => {
+    res.send("User endpoint OK")
 })
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -3,101 +3,104 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const User = require('./models/userModel');
+const User = require('./models/user');
+const routes = require('./routes/routes');
 
 const PORT = 3000
-const api = require('./routes/routes');
 const app = express();
 
 //allows the application to receive json data
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cors({
-    credentials:true,
+    credentials: true,
     origin: ['http://localhost:4200']
 }
-
 ));
+
 app.use(cookieParser());
+
+app.use("/api", routes);
 
 
 //Connect to MongoDB
 mongoose.connect('mongodb+srv://morian:x9zory5ZpLtFbAP3@moriandb.wqca4nl.mongodb.net/MorianDB?retryWrites=true&w=majority')
-.then(() => {
+    .then(() => {
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port: ${PORT}`)
+        app.listen(PORT, () => {
+            console.log(`Server running on port: ${PORT}`)
+        })
+        console.log('Connected to MongoDB')
+    }).catch((err) => {
+        console.log(err)
     })
-    console.log('Connected to MongoDB')
-}).catch((err) =>{
-    console.log(err)
-})
 
 
-app.use('/api', api)
 app.get('/', (req, res) => {
-    res.send('Hello from the only server!')
+    res.send('Hello to CLIENT!')
 })
 
 //users (plural) since we are fetching a multiple users at a time
-app.get('/users', async(req, res) => {
+app.get('/users', async (req, res) => {
     try {
         const users = await User.find({});
         res.status(200).json(users)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
-    
+
 })
 
 //fetch a single user
-app.get('/users/:id', async(req, res) => {
+app.get('/users/:id', async (req, res) => {
     try {
         const id = req.params.id
         const user = await User.findById(id)
         res.status(200).json(user)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 //user (single) since we are saving a single user at a time
-app.post('/users', async(req, res) => {
+app.post('/users', async (req, res) => {
     try {
         const user = await User.create(req.body)
         res.status(200).json(user)
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 //update a user
-app.put('/users/:id', async(req, res) => {
+app.put('/users/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const user = await User.findByIdAndUpdate(id, req.body);
-        if(!user) {
-            return res.status(404).json({message: `User with ID ${id} not found`})
+        if (!user) {
+            return res.status(404).json({ message: `User with ID ${id} not found` })
         }
         const updatedUser = await User.findById(id);
         res.status(200).json(updatedUser);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 //delete a user
-app.delete('/users/:id', async(req, res) => {
+app.delete('/users/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const user = await User.findByIdAndDelete(id);
-        if(!user) {
-            return res.status(404).json({message: `User with ID ${id} not found`})
+        if (!user) {
+            return res.status(404).json({ message: `User with ID ${id} not found` })
         }
         res.status(200).json(user);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
