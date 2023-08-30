96640396a88e6a1ee8d9f02567168b9feca0b0d5
Brian Mbugua <brianmbugua505@gmail.com>
2023-08-30 18:38:32 +0300

    Created register form, Homepage frontend with minor css and bootstrap styling

--- client/viewpesa/src/app/components/register/register.component.ts
+++ client/viewpesa/src/app/components/register/register.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
-}
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ){
+  }
+
+    ngOnInit():void{
+      this.form = this.formBuilder.group({
+        username:"",
+        email: "",
+        password: ""
+      })
+    }
+
+    submit():void{
+      let user = this.form.getRawValue();
+      console.log(user);
+
+      if(user.name == "" || user.email == "" || user.password == ""){
+        Swal.fire("Error", "Please enter all the required fields", "error")
+      }
+    }
+  }
+
