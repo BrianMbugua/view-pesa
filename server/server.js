a72b09c0411938a7a9f516e112a6bec15fea7a9e
Brian Mbugua <brianmbugua505@gmail.com>
2023-08-30 10:57:56 +0300

    Fetch data from database

--- server/server.js
+++ server/server.js
@@ -16,6 +16,18 @@ app.use('/api', api)
 app.get('/', (req, res) => {
     res.send('Hello from the only server!')
 })
+
+app.get('/users', async(req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+    
+})
+
+
 app.post('/users', async(req, res) => {
     try {
         const user = await User.create(req.body)
