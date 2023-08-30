ddd596d92f93c2c519c274cac063c17ad60a34c3
Brian Mbugua <brianmbugua505@gmail.com>
2023-08-30 11:15:29 +0300

    Fetch a single user from DB

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
     res.send('Hello from the only server!')
 })
 
+//users (plural) since we are fetching a multiple users at a time
 app.get('/users', async(req, res) => {
     try {
         const users = await User.find({});
@@ -27,7 +28,18 @@ app.get('/users', async(req, res) => {
     
 })
 
+//fetch a single user
+app.get('/users/:id', async(req, res) => {
+    try {
+        const id = req.params.id
+        const user = await User.findById(id)
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
+//user (single) since we are saving a single user at a time
 app.post('/users', async(req, res) => {
     try {
         const user = await User.create(req.body)
