6746257d1d40041f165e43cdbe5ec1e7131de2f8
Brian Mbugua <brianmbugua505@gmail.com>
2023-09-16 12:12:26 +0300

    add transaction

--- client/viewpesa/src/app/components/transactions/transactions.component.ts
+++ client/viewpesa/src/app/components/transactions/transactions.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, map } from 'rxjs';
 import Swal from 'sweetalert2';
 import { DataService } from 'src/app/services/data.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-transactions',
@@ -11,17 +12,54 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./transactions.component.css']
 })
 export class TransactionsComponent {
-
+  form: FormGroup;
   transactionData: any;
   transactionData$: any;
-  constructor(private dataService: DataService){
+  showForm: boolean = false;
+
+  constructor(private dataService: DataService,
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router) {
 
   }
 
-  
+
   ngOnInit(): void {
     this.transactionData$ = this.dataService.getTransaction()
-    
+    this.form = this.formBuilder.group({
+      category: "",
+      amount: "",
+      date: "",
+      description: ""
+    })
+  }
+
+  errorMessage(): void {
+    Swal.fire("Error", "Error", "error")
+  }
+
+  showTransactionForm(): void {
+    this.showForm = true;
+  }
+  hideTransactionForm(): void {
+    this.showForm = false;
+  }
+
+  submit(): void {
+    let transaction = this.form.getRawValue();
+    console.log(transaction);
+
+    if (transaction.category == "" || transaction.amount == "" || transaction.date == "") {
+      Swal.fire("Error", "Please enter all required transaction details", "error")
+    } else {
+      this.http.post("http://localhost:4000/api/transactions", transaction, { withCredentials: true })
+        .subscribe(() => this.router.navigate(['/transactions']))
+      Swal.fire("Success", "Transactions successfully added", "success"),
+        (err: any) => {
+          Swal.fire("Error", err.error.message, "error")
+        }
+    }
   }
 
 }
